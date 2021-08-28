551639f16a51eb5b1731cac5ae86ab2f30af51c9
daniel <jmzwcn@gmail.com>
2021-08-28 23:45:22 +0800

    mysql json support.
    Signed-off-by: daniel <jmzwcn@gmail.com>

---
+++ db.go
@@ -0,0 +1,196 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
+
+var DB *sql.DB
+
+func InitDB(db *sql.DB) {
+	DB = db
+}
+
+func checkTable(table string) error {
+	sql := "CREATE TABLE IF NOT EXISTS " + table + " (data JSON)"
+	if _, err := DB.Exec(sql); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Insert(table string, obj proto.Message) error {
+	tx, err := DB.Begin()
+	if err != nil {
+		return err
+	}
+	if err := InsertTx(tx, table, obj); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func InsertTx(tx *sql.Tx, table string, obj proto.Message) error {
+	checkTable(table)
+	jsonv, err := protojson.Marshal(obj)
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec("INSERT INTO "+table+"(data) VALUES (CAST(? AS JSON))", jsonv)
+	return err
+}
+
+func InsertIfNotExist(table string, id interface{}, obj proto.Message) error {
+	if GetById(table, id, proto.Clone(obj)) == sql.ErrNoRows {
+		return Insert(table, obj)
+	}
+	return nil
+}
+
+// Update||Insert
+func Upsert(table string, id interface{}, obj proto.Message) error {
+	if err := GetById(table, id, proto.Clone(obj)); err == sql.ErrNoRows {
+		return Insert(table, obj)
+	}
+	return Update(table, id, obj)
+}
+
+func GetById(table string, id interface{}, obj proto.Message) error {
+	return Get(table, map[string]interface{}{"$.id": id}, obj)
+}
+
+// https://dev.mysql.com/doc/refman/5.7/en/json.html#json-paths
+func Get(table string, kvs map[string]interface{}, obj proto.Message) error {
+	checkTable(table)
+	var (
+		keys   []string
+		values []interface{}
+	)
+	for k, v := range kvs { // key should be [json-path], e.g:$.id
+		keys = append(keys, "data->'"+k+"'=?")
+		values = append(values, v)
+	}
+	query := "SELECT data FROM " + table + " WHERE " + strings.Join(keys, " AND ")
+	data := ""
+	if err := DB.QueryRow(query, values...).Scan(&data); err == nil {
+		if err := protojson.Unmarshal([]byte(data), obj); err != nil {
+			return err
+		}
+	} else {
+		return err
+	}
+	return nil
+}
+
+func List(table string, result interface{}, clause ...string) error {
+	checkTable(table)
+	resultv := reflect.ValueOf(result)
+	if resultv.Kind() != reflect.Ptr || resultv.Elem().Kind() != reflect.Slice {
+		return fmt.Errorf("result argument must be a slice address")
+	}
+	slicev := resultv.Elem()
+	elemt := slicev.Type().Elem()
+	query := "SELECT data FROM " + table
+
+	if len(clause) > 0 {
+		for _, v := range clause {
+			query = query + " " + v
+		}
+	}
+	log.Infoln(query)
+	rows, err := DB.Query(query)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	i := 0
+	for rows.Next() {
+		data := ""
+		err := rows.Scan(&data)
+		if err != nil {
+			return err
+		}
+		elemp := reflect.New(elemt.Elem())
+		protojson.Unmarshal([]byte(data), elemp.Interface().(proto.Message))
+		slicev = reflect.Append(slicev, elemp)
+		i++
+	}
+	resultv.Elem().Set(slicev.Slice(0, i))
+	return nil
+}
+
+func Update(table string, id interface{}, newObj proto.Message) error {
+	tx, err := DB.Begin()
+	if err != nil {
+		return err
+	}
+
+	if err := UpdateTx(tx, table, id, newObj); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func UpdateTx(tx *sql.Tx, table string, id interface{}, newObj proto.Message) error {
+	jsonv, err := protojson.Marshal(newObj)
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec("UPDATE "+table+" SET data=CAST(? AS JSON) WHERE data->'$.id'=?", jsonv, id)
+
+	return err
+}
+
+func UpdateKVS(table string, id interface{}, kvs map[string]interface{}) error {
+	var (
+		keys   []string
+		values []interface{}
+	)
+	for k, v := range kvs { // key should be [json-path], e.g:$.id
+		keys = append(keys, ",'"+k+"',?")
+		values = append(values, v)
+	}
+	sql := "UPDATE " + table + " SET data=" + "JSON_SET(data" + strings.Join(keys, "") + ") WHERE data->'$.id'= ?"
+	_, err := DB.Exec(sql, append(values, id)...)
+	return err
+}
+
+func Delete(table string, id interface{}) error {
+	tx, err := DB.Begin()
+	if err != nil {
+		return err
+	}
+	if err := DeleteTx(tx, table, id); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func DeleteTx(tx *sql.Tx, table string, id interface{}) error {
+	_, err := tx.Exec("DELETE FROM "+table+" WHERE data->'$.id' = ?", id)
+	return err
+}
---
+++ db_test.go
@@ -0,0 +1,54 @@
+package db
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	pb "github.com/emart.io/zbay/service/go"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+func TestCast(t *testing.T) {
+	fmt.Println("beign")
+	var msg proto.Message
+	msg = &pb.Account{Id: "abc"}
+	a := msg.ProtoReflect().New()
+	aa := proto.Clone(msg)
+	fmt.Println(msg.(proto.Message), a.(protoreflect.Message), aa)
+	t.Error("done")
+}
+
+func TestPrimaryKey(t *testing.T) {
+	//checkTable("test")
+	//DB.Exec("drop table test")
+}
+
+func TestJsonEscape(t *testing.T) {
+	goods := Goods{Id: "iii"}
+	goods.Desc = "~!@#$%^&*()_+{}|:\";',./<>?'"
+	//fmt.Println(ToJSON(goods))
+	// if err := Insert("test1", goods); err != nil {
+	// 	t.Error(err)
+	// }
+	var g1 Goods
+	// if err := GetById("test1", "iii", &g1); err != nil {
+	// 	t.Error(err)
+	// }
+	fmt.Println(g1.Desc)
+	//DB.Exec("drop table test1")
+	t.Error("done")
+}
+
+type Goods struct {
+	Id   string `json:"id,omitempty"`
+	Name string
+	Desc string
+}
+
+func TestRegexp(t *testing.T) {
+	matched, err := regexp.MatchString("s.*a.*od", "seafoodrtt")
+	fmt.Println(matched, err)
+	t.Error("end")
+}
